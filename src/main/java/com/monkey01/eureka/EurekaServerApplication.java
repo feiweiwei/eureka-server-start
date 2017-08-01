a49d8530b46aa46f9cc32743c218316efc9b5751
feiweiwei <feiweiwei@shrbank.com>
2017-08-01 18:10:13 +0800

    新建eureka工程

---
+++ src/main/java/com/monkey01/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.monkey01.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by feiweiwei on 17/8/1.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
